a2ce71a2e4df0aa0a00f851f70dc135ae2821278
Asian Programmer <parveznew2022@gmail.com>
2025-09-23 17:57:48 +0600

    New Changed

--- src/assets/IconComponents/image.jsx
+++ src/assets/IconComponents/image.jsx
@@ -12,8 +12,7 @@ const ImageIcon = ({ size = 24, stroke = 'currentColor', className = '', ...prop
     strokeLinecap="round"
     strokeLinejoin="round"
     className={className}
-    {...props}
-  >
+    {...props} >
     <rect x="3" y="3" width="18" height="18" rx="2" ry="2" />
     <circle cx="9" cy="9" r="2" />
     <path d="M21 15L17.914 11.914A2 2 0 0 0 15.086 11L6 21" />
