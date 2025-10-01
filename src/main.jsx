3d3cb87dd6a3087187681b6bfa60e920b098b71f
Asian Programmer <parveznew2022@gmail.com>
2025-10-01 18:05:23 +0600

    Table view done

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast"; // ✅ Import Toaster
+
+import 'primereact/resources/themes/lara-dark-indigo/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
 import App from "./App.jsx";
 
 import "./assets/styles/reset.css";
